dc9f613748785e40ea6381a44ae0f863da36e799
Muskan Malhotra <51330645+Muskan-Malhotra@users.noreply.github.com>
2022-10-25 23:14:55 +0530

    Add files via upload

---
+++ typescript/arithmatic.ts
@@ -0,0 +1,29 @@
+function add(n1,n2){
+  return n1+n2;
+}
+function sub(n1,n2){
+  return n1-n2;
+}
+function mult(n1,n2){
+  return n1*n2;
+}
+function div(n1,n2){
+  return n1/n2;
+}
+
+
+var ansAdd: number=0;
+ansAdd=add(5,6);
+console.log("Addition of numbers 5 and 6 gives: "+ansAdd)
+
+var ansSub: number=0;
+ansSub=add(25,16);
+console.log("Subtraction of numbers 16 from 25 gives: "+ansSub)
+
+var ansMult: number=0;
+ansMult=add(5,6);
+console.log("Multiplication of numbers 5 and 6 gives: "+ansMult)
+
+var ansDiv: number=0;
+ansDiv=add(15,3);
+console.log("Division of numbers 15 and 3 gives: "+ansDiv)
---
+++ typescript/enum.ts
@@ -0,0 +1,7 @@
+enum colors {red,green,blue};
+let c:colors=colors.green;
+console.log(c);
+ 
+enum color {red=5,green,blue};
+let d:color=color.red;
+console.log(d)
